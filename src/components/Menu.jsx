d70c7973a8e62a78f2db0e94b14d28de349cbf2a
Gisselle Pombar <gisselle.pombar@gmail.com>
2023-02-03 11:03:22 -0500

    connected to api

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,7 +1,27 @@
+import { useState } from "react";
 export default function Menu(){
+    const [coffees, setCoffees] = useState()
+    const getCoffees = (temp) => {
+        fetch(`https://api.sampleapis.com/coffee/${temp}`)
+        .then(res => res.json())
+        //.then(data => setCoffees(data)) same as below** you are returning res.json() and naming it data and passing it through setCoffees
+        .then(setCoffees)
+        .catch(alert)
+    }
     return (
         <article>
-            <h2>Welcome to the Beau Café</h2>
-        </article>
+            <div>
+                <button onClick={() => getCoffees('hot')}>Hot Coffees</button>
+                <button onClick={() => getCoffees('iced')}>Iced Coffees</button>
+            </div>
+            {!coffees
+                ? <h2>Welcome to the Beau Café</h2>
+                : <ul>
+                    {coffees.map( coffee => (
+                        <li key={coffee.id}>{coffee.title}</li>
+                    ) )}
+                </ul>
+            }
+        </ article>
     );
 }
